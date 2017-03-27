f3102739260550b6ee00356b8cce8bd1f49e0dad
Runzhi <rollink.yang@gmail.com>
2017-03-26 20:57:25 -0400

    show if ph gives left or right chop

--- src/Thinking.java
+++ src/Thinking.java
@@ -2,9 +2,15 @@ public class Thinking implements State {
 
 	@Override
 	public Response recieveRequestFrom(Philosopher philosopher, Request packet, boolean isLeft) {
-		Chopstick leftChop = philosopher.getChopstick(isLeft);
+		Chopstick chopstick = philosopher.getChopstick(isLeft);
 		philosopher.setChopstick(null, isLeft);
-		return new Response(leftChop);
+		if (isLeft && chopstick!=null) {
+			System.out.println("I am giving away left chopstick");
+		}else if (chopstick!=null){
+			System.out.println("I am giving away right chopstick");
+		}
+		
+		return new Response(chopstick);
 	}
 
 	@Override
