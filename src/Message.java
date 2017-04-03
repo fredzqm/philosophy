e1a3dff1db1cd7115d3f29b22c82fa196cf2817c
Fred Zhang <fredzqm@gmail.com>
2017-04-02 20:24:31 -0400

    create four types of Message

--- src/Message.java
+++ src/Message.java
@@ -1,18 +1,43 @@
 import java.io.Serializable;
 
 public class Message implements Serializable {
-
-	public Message() {
-		
-	}
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
 
-	@Override
-	public String toString() {
-		return "Request";
+	public static class ChopstickReqest extends Message {
+		private static final long serialVersionUID = 1L;
 	}
+	public static class BottleReqest extends Message {
+		private static final long serialVersionUID = 1L;
+	}
+	
+	public static class IsAvailableResponse extends Message {
+		private static final long serialVersionUID = 1L;
+		private final boolean isAvailable;
+		
+		public IsAvailableResponse(boolean available) {
+			this.isAvailable = available;
+		}
+
+		public boolean isAvailable() {
+			return isAvailable;
+		}
+	}
+	
+	public static class ChopstickResponse extends IsAvailableResponse {
+		private static final long serialVersionUID = 1L;
+
+		public ChopstickResponse(boolean available) {
+			super(available);
+		}
+	}
+
+	public static class BottleResponse extends IsAvailableResponse {
+		private static final long serialVersionUID = 1L;
+
+		public BottleResponse(boolean available) {
+			super(available);
+		}
+	}
+
 }
