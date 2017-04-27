7190d00420c924d81074abaac5c7e93261dd8a60
RunZGit <yangr@rose-hulman.edu>
2017-04-27 00:11:56 -0400

    lab3 starter code

---
+++ src/main/java/Message.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+
+public abstract class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/Philosopher.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author fredzqm
+ */
+public class Philosopher {
+
+	private static Side left, right;
+
+	public static Side getRight() {
+		return right;
+	}
+
+	public static Side getLeft() {
+		return left;
+	}
+
+	public static Side resolveSide(String ip) {
+		if (ip.equals(left.getIP())) {
+			return left;
+		} else if (ip.equals(right.getIP())) {
+			return right;
+		} else {
+			throw new RuntimeException(
+					String.format("Recieved connection from %s\n\tleft:&s\n\tright:%s\n", ip, left, right));
+		}
+	}
+
+	public static void main(String[] args) {
+		left = new Side(args[0], true);
+		right = new Side(args[1], false);
+
+		FoodManager foodManager = FoodManager.getInstance();
+		BottleManager drinkManager = BottleManager.getInstance();
+
+		Server server = new Server();
+		server.addMessageReciever(foodManager);
+		server.addMessageReciever(drinkManager);
+		server.startServer();
+
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		while (true) {
+			String input = in.nextLine();
+			switch (input) {
+			case "thinking":
+				foodManager.setFoodState(foodManager.new Thinking());
+				drinkManager.setDrinkState(drinkManager.new NotThirsty());
+				break;
+			case "hungry":
+				foodManager.setFoodState(foodManager.new Hungry());
+				break;
+			case "thirsty":
+				drinkManager.setDrinkState(drinkManager.new Thirsty());
+				break;
+			case "notThirsty":
+				drinkManager.setDrinkState(drinkManager.new NotThirsty());
+				break;
+			case "sleep":
+				drinkManager.setDrinkState(drinkManager.new Sleep());
+				break;
+			default:
+				System.out.println("Revieved event: " + input + " current state: " + drinkManager.getDrinkState());
+				break;
+			}
+		}
+	}
+
+}
