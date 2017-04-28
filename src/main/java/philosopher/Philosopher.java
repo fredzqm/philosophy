9bbc507142e685171f319a85bbb0729c209a4e5d
Fred Zhang <fredzqm@gmail.com>
2017-04-27 22:08:03 -0400

    turn on verbose

--- src/main/java/philosopher/Philosopher.java
+++ src/main/java/philosopher/Philosopher.java
@@ -13,7 +13,7 @@ import zookeeper.SideMap;
  */
 public class Philosopher {
 	public static boolean automatic = false;
-	public static boolean verbose = false;
+	public static boolean verbose = true;
 	private Player myself, left, right;
 	private String ip;
 	private PState state;
